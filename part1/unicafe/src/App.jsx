f640d8563d873c66c33e943f14325eed084e34a0
nicolasdev97 <nicolas.po.im@gmail.com>
2026-01-06 12:47:36 -0500

    Exercise 1.8.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Statistics from "./Statistics";
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
@@ -12,13 +13,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {(good / (good + neutral + bad)) * 100}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
